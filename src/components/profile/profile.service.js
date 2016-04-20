39cd6a4e05b12a5b16327b7de6eebd81077f5dbd
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-20 15:09:54 -0600

    Added profile view and service.

---
+++ src/components/profile/profile.service.js
@@ -0,0 +1,25 @@
+angular.module('myApp')
+  .service('ProfileService', ProfileService);
+
+  ProfileService.$inject = ['$http', '$window'];
+
+  function ProfileService ($http, $window) {
+    var baseUrl = 'http://galvanize-student-apis.herokuapp.com/gdating';
+    return {
+      edit: function(data, id) {
+        return $http({
+          method: 'PUT',
+          url: baseUrl + '/members/' + id,
+          data: data,
+        }).then(function(data) {
+          return data;
+        }).catch(function(err) {
+          return err;
+        });
+      },
+      setUserInfo: function(user) {
+        $window.localStorage.setItem('user', JSON.stringify(user.data.data.user));
+        $window.localStorage.setItem('token', JSON.stringify(user.data.data.token));
+      }
+    };
+  }
