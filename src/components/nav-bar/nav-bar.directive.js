c08c31f5cd99eecb381a13dfe0305f78817eec43
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-20 10:18:28 -0600

    Added rootscope currentUser to change nav bar.

--- src/components/nav-bar/nav-bar.directive.js
+++ src/components/nav-bar/nav-bar.directive.js
@@ -1,13 +1,11 @@
 (function() {
   angular.module('myApp')
-    .directive('navBar', [/*'authService', */function(/*authService*/) {
+    .directive('navBar', ['LoginService', function(LoginService) {
       return {
         restrict: 'E',
         templateUrl: '/components/nav-bar/nav-bar.template.html',
-        controller: function($scope) {
-          $scope.message = 'Hello!';
-          // get user info to tell if someone is logged in or not.
-          // $scope.user = authService.getUserInfo();
+        controller: function($scope, $rootScope) {
+          
         }
       };
     }]);
