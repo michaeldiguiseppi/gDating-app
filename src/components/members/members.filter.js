e77242e4bfbfa34340397d1fa2d76ca321d34c83
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-21 12:06:30 -0600

    Added age filter.

--- src/components/members/members.filter.js
+++ src/components/members/members.filter.js
@@ -1,17 +1,18 @@
-angular.module('myApp')
-  .filter('gender', function() {
-    return function(input) {
-      var genders = ['Male', 'Female', 'Other', 'Declined to Answer'];
-      return genders[input];
-    };
-  });
+(function() {
+  angular.module('myApp')
+    .filter('gender', function() {
+      return function(input) {
+        var genders = ['Male', 'Female', 'Other', 'Declined to Answer'];
+        return genders[input];
+      };
+    });
 
   angular.module('myApp')
     .filter('interested', function() {
       return function(input) {
         var output = [];
         var interested = ['Males', 'Females', 'Males or Females', 'Friends Only'];
-        if (input.length > 1) {
+        if (input && input.length > 1) {
           input.forEach(function(interest) {
             output.push(interested[interest]);
           });
@@ -21,3 +22,14 @@ angular.module('myApp')
         }
       };
     });
+  angular.module('myApp')
+    .filter('age', function() {
+      return function(input) {
+        if (!input) { return; }
+        var today = new Date().toString().split(' ')[3];
+        input = input.split('-')[0];
+        return today - input;
+      };
+    });
+
+  })();
