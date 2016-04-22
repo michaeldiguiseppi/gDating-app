eb57cb23c0c708fa07d8461455785b61f5800fd4
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-22 12:06:17 -0600

    Added gender and interested in to profile.:

--- src/components/register/register.controller.js
+++ src/components/register/register.controller.js
@@ -5,17 +5,33 @@
   RegisterCtrl.$inject = ['$scope', 'RegisterService', 'ProfileService', '$state'];
 
   function RegisterCtrl ($scope, RegisterService, ProfileService, $state) {
-    $scope.user = {};
+    $scope.user = {
+     username: '',
+     email: '',
+     password: '',
+     gender: 0,
+     slug: '',
+     names: {
+       firstName: '',
+       lastName: '',
+     },
+     dob: '',
+     address: {
+       zipcode: '',
+       geo: {
+         lat: '',
+         lng: ''
+       }
+     }
+   };
     ProfileService.getCurrentLocation().then(function(data) {
-      $scope.user.lat = data.coords.latitude;
-      $scope.user.lng = data.coords.longitude;
+      $scope.user.address.geo.lat = data.coords.latitude;
+      $scope.user.address.geo.lng = data.coords.longitude;
       $scope.$digest();
     }).catch(function(data) {
       console.log('error', data);
     });
     $scope.register = function() {
-      $scope.user.address.geo.lat = $scope.user.lat;
-      $scope.user.address.geo.lng = $scope.user.lng;
       RegisterService.register($scope.user).then(function(data) {
         console.log(data.data.data);
         if (data.status === 201) {
