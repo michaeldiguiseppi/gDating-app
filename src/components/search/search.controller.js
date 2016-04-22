5a36b0451fb9f5e0929c664fc1fe9b172b5a484c
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-04-22 12:07:19 -0600

    Added search routes.

--- src/components/search/search.controller.js
+++ src/components/search/search.controller.js
@@ -1,15 +1,28 @@
 (function() {
   angular.module('myApp')
-    .controller('SearchCtrl', ['$scope', 'SearchService', function($scope, SearchService) {
+    .controller('SearchCtrl', ['$scope', '$state', 'SearchService', function($scope, $state, SearchService) {
       var ages = [];
       for (var i = 18; i < 90; i++) {
         ages.push(i);
       }
       $scope.ages = ages;
       $scope.search = {};
-      $scope.search.searchType = 'OR';
+      $scope.search.exclusive = 'false';
       $scope.searchFunc = function() {
-        console.log(this.search);
+        SearchService.search($scope.search).then(function(data) {
+          $scope.searchResults = data.data.data;
+          $scope.showReset = true;
+        });
+      };
+      $scope.getOne = function(slug) {
+        SearchService.getOne(slug).then(function(data) {
+          $state.go('members.view', {slug: slug});
+        });
+      };
+      $scope.resetSearch = function() {
+        $scope.search = {};
+        $scope.searchResults = null;
+        $scope.showReset = false;
       };
     }]);
 })();
--- src/components/search/search.service.js
+++ src/components/search/search.service.js
@@ -1,6 +1,21 @@
 (function() {
   angular.module('myApp')
-    .service('SearchService', ['$http', function($http) {
-      
+    .service('SearchService', ['crudService', function(crudService) {
+      return {
+        search: function(form) {
+          return crudService.search('members', form).then(function(data) {
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        },
+        getOne: function(slug) {
+          return crudService.getOne('members', slug).then(function(data) {
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        }
+      };
     }]);
 })();
